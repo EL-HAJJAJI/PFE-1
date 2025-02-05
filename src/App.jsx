0485a675a1ff7cbe1f401861c6246758b39d4273
7aviim <hat.elhajjaji@edu.umi.ac.ma>
2025-02-05 23:16:23 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import Home from './Components/Images/Pages/Home';
 
 function App() {
--- src/Components/Images/Pages/Home.jsx
+++ src/Components/Images/Pages/Home.jsx
@@ -1,11 +1,10 @@
-import 'C:/Users/My-Pc/Desktop/bootsrap-project/src/App.css'
 const Home = () => {
     return (
         <>  {/* NAVBAR */}
             <header>
                 <nav class="navbar navbar-expand-lg navbar-dark bg-black">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#" style={{ fontWeight: "bold" }}>Car-Wow</a>
+                        <a class="navbar-brand" href="#" style={{ fontWeight: "bold" }}>Garage-Essalam</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
